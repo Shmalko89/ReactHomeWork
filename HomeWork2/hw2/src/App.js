94383edac24eca56ecbb5208e3de7b0858641538
Vladimir <shmalko89@list.ru>
2022-08-21 13:57:18 +0400

    update

--- HomeWork2/hw2/src/App.js
+++ HomeWork2/hw2/src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
 
+const messageList = [
+  {
+    text: 'Привет!',
+    author: 'Я',
+  }
+];
+
+
 function App() {
+  const [messages, setMessages] = useState(messageList);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (messages[messages.length - 1]?.author === "Пользователь") {
+        setMessages(prevMessages => [
+          ...prevMessages,
+          { text: "Привет, это Бот", author: "Bot" },
+        ]);
+      }
+    }, 1500);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [messages]);
+
+  const handleMessage = (e) => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: "new message", author: "Пользователь" },
+    ]);
+    e.preventDefault();
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  }
+
+  const BotMessage = ({ text }) => <div> {text} </div>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleMessage}>
+        <button type="submit">Send</button>
+        <input value={value} type="text" onChange={handleChange} />
+        {messages.map((message, i) => (
+          <BotMessage key={i} text={message.text} />
+        ))}
+      </form>
+
     </div>
   );
 }
