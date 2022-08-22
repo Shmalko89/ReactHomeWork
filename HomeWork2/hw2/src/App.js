13f058b2773e7529f8daf00c2fc28798dbd120b5
Vladimir <shmalko89@list.ru>
2022-08-22 21:26:44 +0400

    refact 2

--- HomeWork2/hw2/src/App.js
+++ HomeWork2/hw2/src/App.js
@@ -1,59 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
-
-const messageList = [
-  {
-    text: 'Привет!',
-    author: 'Я',
-  }
-];
-
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [messages, setMessages] = useState(messageList);
-  const [value, setValue] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [messageBody, setMessageBody] = useState({
+    text: '',
+    author: ''
+  })
 
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (messages[messages.length - 1]?.author === "Пользователь") {
-        setMessages(prevMessages => [
-          ...prevMessages,
-          { text: "Привет, это Бот", author: "Bot" },
-        ]);
-      }
-    }, 1500);
+  const botMessage = 'Сообщение Бота!';
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [messages]);
-
-  const handleMessage = (e) => {
-    setMessages((prevMessages) => [
-      ...prevMessages,
-      { text: "new message", author: "Пользователь" },
-    ]);
-    e.preventDefault();
-  };
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  }
-
-  const BotMessage = ({ text }) => <div> {text} </div>;
+  useEffect(() =>{
+    if(messageList.length > 0 && messageList.slice(-1)[0].author !== 'robot'){
+      setTimeout(() => {
+        setMessageList(pervstate => [...pervstate, {text: botMessage, author: 'robot'}])
+      }, 1500)
+    }
+  },[messageList])
 
   return (
-    <div className="App">
-      <form onSubmit={handleMessage}>
-        <button type="submit">Send</button>
-        <input value={value} type="text" onChange={handleChange} />
-        {messages.map((message, i) => (
-          <BotMessage key={i} text={message.text} />
-        ))}
-      </form>
-
+    <div className='App'>
+      <Form data ={messageBody} setData = {setMessageBody} setMessage = {setMessageList}></Form>
+      <div className='messageList'>
+        {
+          messageList.map((e, i) => <Message text={e.text} author={e.author} key = {i}/>)
+        }
+      </div>
     </div>
   );
 }
 
 export default App;
+
+const Form = ({data, setData, setMessage}) => {
+
+  const {text, author} = data
+
+  const submitForm = (e) =>{
+    e.preventDefault()
+    if(text.length > 0){
+      setMessage(pervstate => [...pervstate, {text, author}])
+    }
+    setData(
+      {
+        text: '',
+        author: ''
+      }
+    )
+  }
+
+  return(
+    <form onSubmit={submitForm}>
+      <input placeholder='Имя' value={text} onChange = {(e)=>
+      setData(pervstate => ({...pervstate, text: e.target.value})
+      )}/>
+
+      <input placeholder='Текст' value={author} onChange = {(e)=>
+      setData(pervstate => ({...pervstate, author: e.target.value})
+      )}/>
+
+      <button type='submit'>Отправить</button>
+    </form>
+  )
+}
+
+const Message = ({author, text}) =>{
+  return(
+    <div>
+      <hr/>
+      <h1>{author}</h1>
+      <p>{text}</p>
+    </div>
+  )
+}
+  
